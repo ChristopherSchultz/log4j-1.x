8bf2ac79f05e323750f3c054acbe6ad214d0ba20
Christopher Schultz <chris@christopherschultz.net>
2017-02-03 10:11:17 -0500

    Improve code comments. No functional change.

--- java/org/apache/log4j/extras/DelayedTriggeringEventEvaluator.java
+++ java/org/apache/log4j/extras/DelayedTriggeringEventEvaluator.java
@@ -12,16 +12,22 @@ import org.apache.log4j.spi.TriggeringEventEvaluator;
 public class DelayedTriggeringEventEvaluator
     implements TriggeringEventEvaluator
 {
-    // Buffer size of the parent appender is not accessible, so we
-    // have to have our own.
+    /**
+     * Buffer size of the parent appender is not accessible, so we
+     * have to have our own triggering event count.
+     */
     private int _triggeringEventCount = 512;
+
+    /**
+     * The number of events seen since the last triggering event.
+     */
     private int _eventCount = 0;
 
     /**
      * Sets the number of events that must occur before this evaluator
      * indicates that the event is a "triggering event".
      *
-     * @param bufferSize The size
+     * @param events The number of events required to cause this evaluator to trigger.
      */
     public synchronized void setEventCount(int events)
     {
